ade902a6c38563d77c261f234c7ac1d333971f06
Daniel Anton Mata <anton.mata@gmail.com>
2021-05-02 10:24:14 +0800

    🌏 Hello, World!

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Game from './game';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
